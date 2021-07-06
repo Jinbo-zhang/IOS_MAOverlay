7fed3e35dba04a96034b75056f2f51b92b3824fc
zhangjb <1094601269@qq.com>
2021-07-06 18:04:33 +0800

    Initial Commit

---
+++ IOS_MAOverlayTests/IOS_MAOverlayTests.m
@@ -0,0 +1,36 @@
+//
+//  IOS_MAOverlayTests.m
+//  IOS_MAOverlayTests
+//
+//  Created by zhang jb on 2021/7/6.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IOS_MAOverlayTests : XCTestCase
+
+@end
+
+@implementation IOS_MAOverlayTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
